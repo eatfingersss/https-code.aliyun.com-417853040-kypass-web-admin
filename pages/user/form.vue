0e248300dee1698d9cf17d0ff7e23c18613a3a6a
417853040 <417853040@qq.com>
2018-09-11 22:56:34 +0800

    新增user的form页面

---
+++ pages/user/form.vue
@@ -0,0 +1,32 @@
+<template>
+    <!--只能有一个根节点-->
+    <div>
+
+    </div>
+</template>
+<script>
+  //    import xx from 'xx'
+  export default {
+    name: 'form',
+//        组件
+    components: {},
+//        数据
+    data() {
+      return {}
+    },
+//        计算属性
+    computed: {},
+//        方法
+    methods: {},
+//        钩子：发生在路由变化时
+    mounted() {
+    },
+//         监听数据
+    watch: {},
+//         组件参数
+    props: {}
+  }
+</script>
+<style scoped>
+
+</style>
