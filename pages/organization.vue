d9271ad41433d47288a78eaf349fb02a0fcd2f6a
eatfingersss <501847102@qq.com>
2018-09-20 17:38:14 +0800

    组织列表待完成
    欧拉欧拉欧拉欧拉

---
+++ pages/organization.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-tabs type="border-card">
+    <el-tab-pane label="组织管理">
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>aoaoaoaoaoao</el-breadcrumb-item>
+      </el-breadcrumb>
+      <br>
+      <OrganizationList></OrganizationList>
+    </el-tab-pane>
+   
+    <el-tab-pane label="分组管理">
+      <UserForm></UserForm>
+    </el-tab-pane>
+  </el-tabs>
+</template>
+
+<script>
+  import OrganizationList from './organization/organizationList'
+  import OrganizationGroupList from './organization/organizationGroupList'
+
+  export default {
+    name: 'organization',
+//        组件
+    components: {
+      OrganizationList: OrganizationList,
+      OrganizationGroupList: OrganizationGroupList
+    },
+//        数据
+    data() {
+      return {
+      }
+    },
+//        计算属性
+    computed: {},
+//        方法
+    methods: {
+    },
+    mounted() {
+    },
+//         监听数据
+    watch: {},
+//         组件参数
+    props: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/organization/organizationGroupList.vue
@@ -0,0 +1,139 @@
+<template>
+  <div>
+    <el-table
+      :data="tableData"
+      border
+      style="width: 100%">
+      <el-table-column
+        fixed
+        prop="id"
+        label="id"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="分组名">
+      </el-table-column>
+      <el-table-column
+        prop="remark"
+        label="备注">
+      </el-table-column>
+      <el-table-column
+        prop="only"
+        label="组内组织用户是否可重复添加">
+      </el-table-column>
+      <el-table-column
+        prop="status"
+        label="状态">
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="100">
+        <template slot-scope="scope">
+          <el-button type="text" size="small" @click="dialogFormVisible = true">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- Form -->
+<el-dialog title="编辑分组" :visible.sync="dialogFormVisible">
+  <el-form :model="form">
+    <el-form-item label="id" :label-width="formLabelWidth">
+      <el-input v-model="form.name" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="分组名" :label-width="formLabelWidth">
+      <el-input v-model="form.name" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="备注" :label-width="formLabelWidth">
+      <el-input v-model="form.name" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="组内组织用户是否可重复添加" :label-width="formLabelWidth">
+      <el-input v-model="form.name" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="状态" :label-width="formLabelWidth">
+      <el-input v-model="form.name" autocomplete="off"></el-input>
+    </el-form-item>
+    <!--<el-form-item label="账户类型" :label-width="formLabelWidth">
+      <el-select v-model="form.region" placeholder="请选择账户类型">
+        <el-option label="超级管理员" value="shanghai"></el-option>
+        <el-option label="学生" value="beijing"></el-option>
+        <el-option label="教师" value="beijing"></el-option>
+      </el-select>
+    </el-form-item>-->
+    <el-form-item label="修改状态" :label-width="formLabelWidth">
+      <el-button>冻结账号</el-button>
+    </el-form-item>
+  </el-form>
+  <div slot="footer" class="dialog-footer">
+    <el-button @click="dialogFormVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+  </div>
+</el-dialog>
+
+  </div>
+</template>
+
+<script>
+  import requestApi from './../../assets/js/requestApi'
+  export default {
+    name: 'organizationGroupList',
+
+    data() {
+      return {
+        tableData: [],
+        dialogFormVisible: false,
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        formLabelWidth: '120px'
+      }
+    },
+    methods: {
+      organizationGroupList: async function () {
+        let obj = await requestApi.getRequest('/api/organization/organizationGroupList', this)
+        this.tableData = obj.result
+        for (let item of this.tableData) {
+          if (item.type === 1) {
+            item.type = '超级管理员'
+          } else if (item.type === 2) {
+            item.type = '教师'
+          } else if (item.type === 3) {
+            item.type = '学生'
+          } else {
+            item.type = '未知'
+          }
+
+          if (item.status === 0) {
+            item.status = '未激活'
+          } else if (item.status === 1) {
+            item.status = '正常'
+          } else if (item.status === -1) {
+            item.status = '锁定'
+          } else if (item.status === -2) {
+            item.status = '冻结'
+          } else if (item.status === -3) {
+            item.status = '删除'
+          } else {
+            item.status = '未知'
+          }
+        }
+      }
+    },
+    mounted() {
+      console.log('33')
+      this.organizationGroupList()
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
